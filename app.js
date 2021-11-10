e8dd36b5250d74c01439b8e6dc7194c9785886d5
ameyacharnalia <ameya.charnalia@gmail.com>
2021-11-09 20:48:53 -0500

    Initial repo post

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const blogRoutes = require('./routes/blogRoutes')
+require('dotenv').config()
+
+// Connect to MongoDB and creating the server until .then runs
+const dbURI = 'mongodb+srv://ameyayeg:rldl33JwBNK0A491@nodeblogs.a4sxe.mongodb.net/nodeblogs?retryWrites=true&w=majority'
+mongoose.connect(process.env.dbURI, {useNewUrlParser: true, useUnifiedTopology:true })
+    .then((res) => app.listen(8080))
+    .catch((err) => console.log(err))
+
+// Register view engine
+app.set('view engine', 'ejs')
+
+// Static files and middleware
+app.use(express.static('./public'))
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+
+// Routes 
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About'})
+})
+
+// Blog routes
+
+app.use('/blogs', blogRoutes)
+
+// 404
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404'})
+})
+
