d9c4450cd6ad07aa2b8cd97f39933e4a8ca7b7ed
ameyacharnalia <ameya.charnalia@gmail.com>
2021-11-18 15:11:02 -0500

    Fixed login form stylings

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(
     session({
-      secret: 'secret',
+      secret: process.env.SECRET,
       resave: true,
       saveUninitialized: true
     })
